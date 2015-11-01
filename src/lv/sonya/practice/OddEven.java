54c1a6105e5a370662457a2a322a90752987ef4f
raznaya <sonya.bolotnikova@gmail.com>
2015-11-01 19:16:45 +0200

    Update OddEven.java

--- src/lv/sonya/practice/OddEven.java
+++ src/lv/sonya/practice/OddEven.java
@@ -3,20 +3,38 @@ package lv.sonya.practice;
 public class OddEven {
 
 	public static void main(String[] args) {
-		int[] numbers = {1,2,3,4,5,6,0,7,8,9,10};
+		int[] numbers = new int[5];
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Input 5 positive numbers!");
+		for (int i = 0; i < 5; i++) {
+			
+			boolean flag = true;
+			while(flag) {
+				try {
+					numbers[i] = scanner.nextInt();
+					if (numbers[i] == 0){
+						throw new IllegalArgumentException("Number must be greater than 0!");
+					}
+					flag = false;
+				} catch(IllegalArgumentException e) {
+//					e.printStackTrace();
+					System.out.println(e.getMessage());
+				}
+			}
+			
+		}
 		int even = 0;
 		int odd = 0;
-		for (int number : numbers){
-			if (number == 0){
-			break;
-			}
-			if (number % 2 == 0){
+		for (int number : numbers) {
+			if (number % 2 == 0) {
 				odd++;
-			} else { 
+			} else {
 				even++;
 			}
+			
 		}
 		System.out.println("There are " + odd + " odd numbers and " + even + " even numbers");
+		scanner.close();
 	}
 
 }
