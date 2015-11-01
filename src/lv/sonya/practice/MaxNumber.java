08de1865fceeb84737003c2c6f673b25a9dfd93b
raznaya <sonya.bolotnikova@gmail.com>
2015-11-01 18:29:48 +0200

    Update MaxNumber.java

--- src/lv/sonya/practice/MaxNumber.java
+++ src/lv/sonya/practice/MaxNumber.java
@@ -3,25 +3,30 @@ package lv.sonya.practice;
 public class MaxNumber {
 
 	public static void main(String[] args) {
-		int number1 = 15;
-		int number2 = 8;
-		int number3 = 47;
-		int maxnumber;
-		
-		maxnumber = number1;
-		if(maxnumber >2 && maxnumber > number3){
-			System.out.println("Number 1 is the greatest: " + maxnumber);
-		} else {
-			maxnumber = number2;
-			if (maxnumber > number1 && maxnumber > number3) {
-				System.out.println("Number 2 is the greatest: " + maxnumber);
+			int number1 = 3;
+			int number2 = 8;
+			int number3 = 1;
+			int maxnumber;
+			
+			while(true){
+			if (number1 == number2 && number2 == number3){
+				System.out.println("Numbers are equal.");
+				break;
+			}
+			maxnumber = number1;
+			if(maxnumber > number2 && maxnumber > number3){
 			} else {
-				maxnumber = number3;
-				if ( maxnumber > number1 && maxnumber > number2) {
-					System.out.println("Number 3 is the greatest: " + maxnumber);
+				maxnumber = number2;
+				if (maxnumber > number1 && maxnumber > number3) {
+				} else {
+					maxnumber = number3;
+					if ( maxnumber > number1 && maxnumber > number2) {
+					}
 				}
 			}
+			System.out.println("The greatest number is: " + maxnumber);
+			break;
 		}
 	}
-	//Programma rabotaet, eto ja napisala sama, bez podskazok :)
 }
+
