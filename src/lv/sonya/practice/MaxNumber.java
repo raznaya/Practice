0a40038fd00aa1cf417e2ae3b7ceef2d80aedc6a
raznaya <sonya.bolotnikova@gmail.com>
2015-11-04 22:38:35 +0200

    Improved MaxNumber formatting.

--- src/lv/sonya/practice/MaxNumber.java
+++ src/lv/sonya/practice/MaxNumber.java
@@ -7,9 +7,9 @@ public class MaxNumber {
 		int number2 = 8;
 		int number3 = 47;
 		int maxnumber;
-		
+
 		maxnumber = number1;
-		if(maxnumber >2 && maxnumber > number3){
+		if (maxnumber > number2 && maxnumber > number3) {
 			System.out.println("Number 1 is the greatest: " + maxnumber);
 		} else {
 			maxnumber = number2;
@@ -17,11 +17,11 @@ public class MaxNumber {
 				System.out.println("Number 2 is the greatest: " + maxnumber);
 			} else {
 				maxnumber = number3;
-				if ( maxnumber > number1 && maxnumber > number2) {
+				if (maxnumber > number1 && maxnumber > number2) {
 					System.out.println("Number 3 is the greatest: " + maxnumber);
 				}
 			}
 		}
 	}
-	//Programma rabotaet, eto ja napisala sama, bez podskazok :)
+	// Programma rabotaet, eto ja napisala sama, bez podskazok :)
 }
