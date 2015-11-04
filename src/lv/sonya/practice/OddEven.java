ec29c107395e2c85cedd42acdd1c4f917643d429
raznaya <sonya.bolotnikova@gmail.com>
2015-11-04 22:47:13 +0200

    Fixed odd even.

--- src/lv/sonya/practice/OddEven.java
+++ src/lv/sonya/practice/OddEven.java
@@ -1,5 +1,7 @@
 package lv.sonya.practice;
 
+import java.util.Scanner;
+
 public class OddEven {
 
 	public static void main(String[] args) {
@@ -7,21 +9,21 @@ public class OddEven {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Input 5 positive numbers!");
 		for (int i = 0; i < 5; i++) {
-			
+
 			boolean flag = true;
-			while(flag) {
+			while (flag) {
 				try {
 					numbers[i] = scanner.nextInt();
-					if (numbers[i] == 0){
+					if (numbers[i] == 0) {
 						throw new IllegalArgumentException("Number must be greater than 0!");
 					}
 					flag = false;
-				} catch(IllegalArgumentException e) {
-//					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					// e.printStackTrace();
 					System.out.println(e.getMessage());
 				}
 			}
-			
+
 		}
 		int even = 0;
 		int odd = 0;
@@ -31,7 +33,7 @@ public class OddEven {
 			} else {
 				even++;
 			}
-			
+
 		}
 		System.out.println("There are " + odd + " odd numbers and " + even + " even numbers");
 		scanner.close();
