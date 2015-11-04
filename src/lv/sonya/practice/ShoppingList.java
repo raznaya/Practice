c07e81653b0bf0da9d044f1c725db03bf8d04695
raznaya <sonya.bolotnikova@gmail.com>
2015-11-04 22:50:12 +0200

    Renamed shopping list to java.

--- src/lv/sonya/practice/ShoppingList.java
+++ src/lv/sonya/practice/ShoppingList.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+package lv.sonya.practice;
+
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ShoppingList {
 
 	public static void main(String[] args) {
-		System.out.println("Hello! I`m am Shopping List Program! Add some items, their cost and I`ll sum the total amount of money you need to spend.");
+		System.out.println(
+				"Hello! I`m am Shopping List Program! Add some items, their cost and I`ll sum the total amount of money you need to spend.");
 		Scanner scanner = new Scanner(System.in);
 		String[] nameArray = new String[5];
 		int[] costArray = new int[5];
-		
-		for(int a = 0; a < 5; a++){
+
+		for (int a = 0; a < 5; a++) {
 			nameArray[a] = scanner.nextLine();
 			costArray[a] = scanner.nextInt();
-			}
+		}
 		System.out.print(Arrays.toString(nameArray));
 		System.out.print(Arrays.toString(costArray));
 		scanner.close();
-		}
-		}
-		
+	}
+}
