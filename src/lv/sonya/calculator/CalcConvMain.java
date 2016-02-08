c663d9f1239e7195e412a6900c3d2f1e9becca43
Sofija <sonya.bolotnikova@gmail.com>
2016-02-08 16:34:40 +0200

    Simple converter added.

--- src/lv/sonya/calculator/CalcConvMain.java
+++ src/lv/sonya/calculator/CalcConvMain.java
@@ -6,17 +6,29 @@ public class CalcConvMain {
 	static User user = new User();
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to calculator and converter program!");
-		
-		Calculator calculator = new Calculator(0, 0);
-		
-		while(true){
-		System.out.println("Write down operation!");
-		calculator.performCalculation();
-		}
-	
+		System.out.println("Welcome to calculator and converter program, select calc or conv!");
+		chooseConvOrCalc();
 }
 	
+	
+	public static void chooseConvOrCalc(){
+		String text = User.userInput();
+		if(text.equals("calculator")){
+			Calculator calculator = new Calculator(0, 0);
+			while(true){
+				System.out.println("Write down operation!");
+				calculator.performCalculation();
+				}
+		} else {
+			if(text.equals("converter")){
+				Converter converter = new Converter(0);
+				while(true){
+					System.out.println("Write down operation!");
+					converter.performConvertion();
+					}
+			}
+		}
+	}
 	
 	
 	
--- src/lv/sonya/calculator/Converter.java
+++ src/lv/sonya/calculator/Converter.java
@@ -2,7 +2,37 @@ package lv.sonya.calculator;
 
 public class Converter {
 
-	public static void mainForConverter(String[] args) {
-		System.out.println("Coming soon...");
+	private static double numberToConvert;
+	private static String[] parts;
+	private static String whatToConvert;
+	private static String toWhatConvert;
+
+	public Converter( double numberToConvert){
+		Converter.numberToConvert = numberToConvert;
+	}
+	
+	public double meterToMilimeters() {
+		return numberToConvert * 1000;
+	}
+	
+	public static void operationInput() {
+		
+		String input = User.userInput();
+		parts = input.split(" ");
+		numberToConvert = Double.parseDouble(parts[0]);
+		whatToConvert = parts[1];
+		toWhatConvert = parts[3];
+	}
+	
+	public double extractOperation(){
+		double result = 0;
+		if(whatToConvert.equals("m") && toWhatConvert.equals("mm")){
+			result = meterToMilimeters();
+		} return result;
+	}
+	
+	public void performConvertion(){
+		operationInput();
+		System.out.println("= " + extractOperation());
 	}
 }
