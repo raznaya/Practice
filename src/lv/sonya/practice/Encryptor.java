d90359798b48b1c5ea724bd18449a09f7fdb1557
Sofija <sonya.bolotnikova@gmail.com>
2016-02-19 14:29:21 +0200

    Encryptor

--- src/lv/sonya/practice/Encryptor.java
+++ src/lv/sonya/practice/Encryptor.java
@@ -1,29 +1,57 @@
 package lv.sonya.practice;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Encryptor {
-	
+
 	static String text;
+	String[] temporary;
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		text = scanner.nextLine();
 		Encryptor encryptor = new Encryptor();
-		encryptor.encrypt(text);
-
+		System.out.println(encryptor.encrypt(text));
+		String textForDecoding = scanner.nextLine();
+		System.out.println(encryptor.decode(textForDecoding));
 	}
-	
-	public void encrypt(String input){
+
+	public ArrayList<String> encrypt(String input) {
 		String reversedText = new StringBuilder(input).reverse().toString();
-		System.out.println(reversedText);
-		String[] temporary = reversedText.split("");
-		for(int i : temporary[]){
-			
+		temporary = reversedText.split("");
+		ArrayList<String> encryptedText = new ArrayList<String>();
+		for (String letter : temporary) {
+			encryptedText.add(letter);
+			if (letter.equals("a")) {
+				encryptedText.add("8");
+				encryptedText.remove("a");
+			}
+			if (letter.equals("s")) {
+				encryptedText.add("3");
+				encryptedText.remove("s");
+			}
 		}
+		return encryptedText;
 	}
 
+	public ArrayList<String> decode(String input) {
+		ArrayList<String> decodedText = new ArrayList<>();
+		temporary = input.split("");
+		for( String letter : temporary){
+			decodedText.add(letter);
+			if(letter.equals("8")){
+				decodedText.add("a");
+				decodedText.remove("8");
+			}
+			if(letter.equals("3")){
+				decodedText.add("s");
+				decodedText.remove("3");
+			}
+		} return decodedText;
+	}
 }
