fcc8f4629224f4036e8758e0d350272ce82353a1
raznaya <sonya.bolotnikova@gmail.com>
2016-02-23 13:26:41 +0200

    no message

--- src/lv/sonya/calculator/Calculator.java
+++ src/lv/sonya/calculator/Calculator.java
@@ -7,20 +7,11 @@ import static lv.sonya.calculator.CalcOperations.SUBSTRACTION;
 
 public class Calculator {
 
-<<<<<<< HEAD
 	private String[] parts;
 	private double number1;
 	private double number2;
 
 	public Calculator(double number1, double number2) {
-=======
-	
-	private String[] parts;
-	private double number1;
-	private double number2;
-	
-	public Calculator(double number1, double number2){
->>>>>>> dc1435b4699f97c9154fe51b3672de6efa1614ca
 		this.number1 = number1;
 		this.number2 = number2;
 	}
@@ -41,12 +32,7 @@ public class Calculator {
 		return number1 + number2;
 	}
 
-<<<<<<< HEAD
-	public void operationInput() {
-		String input = User.userInput();
-=======
 	public void performCalculation(String input) {
->>>>>>> dc1435b4699f97c9154fe51b3672de6efa1614ca
 		parts = input.split(" ");
 		number1 = Double.parseDouble(parts[0]);
 		number2 = Double.parseDouble(parts[2]);
